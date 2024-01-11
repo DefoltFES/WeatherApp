91b61538e0a58bb30b74491b7210f259a7d7d965
DefoltFES <mironoff.emil@yandex.ru>
2024-01-11 15:35:07 +0300

    Add project files.

---
+++ WeatherApp/Model/Weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Models
+{
+    public class Weather
+    {
+        public int Tempeture { get; set; }
+        public float SpeedWindow { get; set; }
+        public string Description { get; set; }
+    }
+}
